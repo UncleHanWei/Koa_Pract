3ce5955c07ebfa28c14553e2550ee12add4d0dae
UncleHanWei <a0973809807@gmail.com>
2023-09-01 22:34:38 +0800

    update code

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const render = require("@koa/ejs"); // ejs 模板語言渲染函式庫
 const serve = require("koa-static");  // 提供靜態檔案的函式庫
 const { bodyParser } = require("@koa/bodyparser"); // 把 post request 轉換成可讀取的資料
 const path = require("path");
+const mysql = require('mysql');
+require('dotenv').config();
 
 const app = new Koa();
 
@@ -25,6 +27,19 @@ render(app, {
 
 // setup the logger
 app.use(morgan('dev'));
+
+const pool = mysql.createPool({
+  host            : process.env.DB_HOST,
+  user            : process.env.DB_USER,
+  password        : process.env.DB_PWD,
+  database        : process.env.DB_DB
+});
+
+app.use((ctx, next) => {
+  ctx.db = pool;
+  next();
+});
+
 
 app.use(indexRouter.routes(), indexRouter.allowedMethods());
 app.use(userRouter.routes(), userRouter.allowedMethods());
--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,23 @@ const router = new Router();
 
 router.prefix('/login');
 
+
+const selectUser = (db, userData) => {
+  return new Promise((rs, rj) => {
+    const sql = "SELECT id, uuid, username, password FROM user WHERE username=? AND password=?";
+    const params = [userData['user'], userData['password']];
+    db.query(sql, params, function (error, results, fields) {
+      if (error) throw error;
+      console.log('The result is: ', results[0]);
+    });
+  });
+}
+
 router.post('/', async (ctx, next) => {
   console.log(ctx.request.body);
+
+  let selectRes = await selectUser(ctx.db, ctx.request.body)
+
   ctx.body = 200;
 });
 
