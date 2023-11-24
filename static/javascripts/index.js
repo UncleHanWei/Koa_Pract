037357b8ebb5ca7b60a936d69f910c74727f464a
UncleHanWei <a0973809807@gmail.com>
2023-11-24 21:55:19 +0800

    update

--- static/javascripts/index.js
+++ static/javascripts/index.js
@@ -1,1 +1,51 @@
-alert(123)
+function getUnfinishList() {
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("/api/todo/unfinished", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+      result = JSON.parse(result)
+      let html = ''
+      result.forEach(element => {
+        html += element?.create_time + '<br>'
+        html += element?.due_time + '<br>'
+        html += element?.content + '<br>'
+        html += element?.status ? '已完成' : '未完成' + '<br>'
+      });
+      document.getElementById('unfinished').innerHTML = html
+    })
+    .catch(error => console.log('error', error));
+}
+
+function getFinishList() {
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("/api/todo/finished", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+      result = JSON.parse(result)
+      let html = ''
+      result.forEach(element => {
+        html += element?.create_time + '<br>'
+        html += element?.due_time + '<br>'
+        html += element?.content + '<br>'
+        html += element?.status ? '已完成' : '未完成' + '<br>'
+      });
+      document.getElementById('finished').innerHTML = html
+    })
+    .catch(error => console.log('error', error));
+}
+
+
+function init() {
+  getUnfinishList()
+  getFinishList()
+}
+
+init()
